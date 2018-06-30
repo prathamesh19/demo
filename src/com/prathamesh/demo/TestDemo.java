8f91ea19746eab30334471e8bdb4df13e7c4c599
prathamesh19 <somnatheprathamesh@gmail.com>
2018-06-30 21:15:13 +0530

    first commit w/o changes

---
+++ src/com/prathamesh/demo/TestDemo.java
@@ -0,0 +1,10 @@
+package com.prathamesh.demo;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+
+}
